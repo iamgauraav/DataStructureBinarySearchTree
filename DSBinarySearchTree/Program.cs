fb3a8b46bbbaf2063dcc7b248f030ca7487b8a6f
iamgauraav <iamgauraav@gmail.com>
2022-03-28 23:53:36 +0530

    Added Welcome message to master branch

---
+++ DSBinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to  Data Structure Binary Search Tree on master branch");
+        }
+    }
+}
